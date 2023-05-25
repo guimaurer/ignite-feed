b5c823a67c81c4e14cfff11e1d3b5d7ecce86a65
guimaurer <guilhermemaurer.adm@gmail.com>
2023-05-25 17:37:17 -0300

    feat: Header, Sidebar, Avatar

--- 01-FUNDAMENTOS-REACTJS/src/App.jsx
+++ 01-FUNDAMENTOS-REACTJS/src/App.jsx
@@ -1,11 +1,23 @@
-import { Post } from './Post';
+import { Post } from './components/Post';
+import { Header } from './components/Header';
 
+import styles from './App.module.css'
+import './global.css';
+import { Sidebar} from './components/Sidebar';
 export function App() {
 
   return (
       <>
-     <Post author="Guilherme" content="aqui vai um texto" />
-     <Post author="Guilherme" content="aqui vai um texto" />
+      <Header />
+      <div className={styles.wrapper}>
+
+          <Sidebar />
+
+        <main>
+        <Post author="Guilherme" content="aqui vai um texto" />
+        <Post author="Guilherme" content="aqui vai um texto" />
+        </main>
+      </div>
      </>
   )
 }
