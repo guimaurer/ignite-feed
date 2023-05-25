9d3d0a5f84bdfcec918d496698969b1af827b25c
guimaurer <guilhermemaurer.adm@gmail.com>
2023-05-25 14:09:22 -0300

    init

---
+++ 01-FUNDAMENTOS-REACTJS/src/App.jsx
@@ -0,0 +1,11 @@
+import { Post } from './Post';
+
+export function App() {
+
+  return (
+      <>
+     <Post author="Guilherme" content="aqui vai um texto" />
+     <Post author="Guilherme" content="aqui vai um texto" />
+     </>
+  )
+}
